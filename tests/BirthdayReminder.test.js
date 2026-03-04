036512f56db7a6a64eb6fef580152dce15832c05
maria <mashapacseeva@gmail.com>
2026-03-04 18:14:01 +0700

    Создан тест на получение списка всех друзей

--- tests/BirthdayReminder.test.js
+++ tests/BirthdayReminder.test.js
@@ -14,3 +14,18 @@ test('Класс BirthdayReminder должен позволять добавля
     
     expect(reminder.friends).toContain(friend);
 });
+
+test('Класс BirthdayReminder должен позволять получать список всех добавленных друзей', () => {
+    const reminder = new BirthdayReminder();
+    const friend1 = new Friend('Иван Иванов', '1990-05-15');
+    const friend2 = new Friend('Марина Петрова', '1985-10-20');
+    
+    reminder.addFriend(friend1);
+    reminder.addFriend(friend2);
+    
+    const friends = reminder.getAllFriends();
+    
+    expect(friends).toHaveLength(2);
+    expect(friends).toContain(friend1);
+    expect(friends).toContain(friend2);
+});
