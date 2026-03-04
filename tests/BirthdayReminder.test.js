d4891d72a2d71aafb7cc880192c1645831c0bab7
maria <mashapacseeva@gmail.com>
2026-03-04 18:10:05 +0700

    Создан тест на добавление друзей в список

--- tests/BirthdayReminder.test.js
+++ tests/BirthdayReminder.test.js
@@ -1,4 +1,16 @@
+const BirthdayReminder = require('../BirthdayReminder');
+const Friend = require('../Friend');
+
 test('BirthdayReminder класс должен быть создан', () => {
     const reminder = new BirthdayReminder();
     expect(reminder).toBeDefined();
+});
+
+test('Класс BirthdayReminder должен позволять добавлять друга в список', () => {
+    const reminder = new BirthdayReminder();
+    const friend = new Friend('Иван Иванов', '1990-05-15');
+    
+    reminder.addFriend(friend);
+    
+    expect(reminder.friends).toContain(friend);
 });
