f909d8a25a5190c773ade5014fb85e658b884742
maria <mashapacseeva@gmail.com>
2026-03-04 17:30:10 +0700

    Создан тест на создание Friend с именем и днем рождения

--- tests/Friend.test.js
+++ tests/Friend.test.js
@@ -1,4 +1,6 @@
+const Friend = require('../Friend');
+
 test('Friend класс должен быть создан', () => {
-    const friend = new Friend();
+    const friend = new Friend('Иван Иванов', '1990-05-15');
     expect(friend).toBeDefined();
 });
